97095ca1d65945ced512a172c7173916dfe2c1e5
andres pabon <andresfelipepabon@uts.edu.co>
2022-10-11 11:27:41 -0500

    avance pasarela de pago

---
+++ src/components/Payment/Details.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import style from './Details.module.css'
+
+const Details = () => {
+  return (
+
+    <div className={style.container}>
+      <section className={style.travelDetail}>
+        <img src="https://a0.muscache.com/im/pictures/00adf705-3791-40e9-8512-ca5a7d619c11.jpg?aki_policy=large"
+          alt='travel' />
+
+        <div className={style.travelDescription}>
+          <small>Yurta</small>
+          < h3>Yurtas de jardín forestal</h3>
+          <div>
+            <p>4.90  (103 reseñas) <p> Superanfitrión </p></p>
+          </div>
+        </div>
+      </section>
+
+      
+      <p className={style.lineaDivide}>Tu reservación cuenta con la protección de AirCover</p>
+      <section className={style.priceInformation}>
+        <h3 >Información del precio</h3>
+        <p> <span className= {style.priceLene}>$439,048.09 COP x 2 noches </span> $878,096.18 COP </p>
+        <p> <span className= {style.priceLene}> Tarifa de limpieza    </span>        $288,263.90 COP  </p>
+        <p> <span className= {style.priceLene}>Tarifa por servicio   </span>      $288,263.90 COP</p>
+        
+      </section>
+      <p className={style.totalPrice}>Total (COP)  <span className= {style.priceLene}> $1,400,696.46 </span></p>
+               </div>
+
+  )
+}
+export default Details
---
+++ src/components/Payment/TripDetails.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+const TripDetails = () => { 
+    return (
+        <section>
+            <div> 
+               <span>
+               <h4> ¡Buen precion!</h4>
+               <p> El precio en las fechas que elegiste es $86,210 COP más bajo que la tarifa promedio por noche de los últimos 3 meses.</p>
+               </span>
+             <p>icono </p>
+            </div>
+            <h3>Tu vieje</h3>
+             <div>
+                <h5>Fechas</h5>
+
+             </div>
+        </section>
+    )
+
+}
+
+export default TripDetails
