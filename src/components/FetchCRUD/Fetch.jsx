128defa00207aec8ba0231d3e73be2992cec405e
andres pabon <andresfelipepabon@uts.edu.co>
2022-10-16 17:08:09 -0500

    correction URL  API

--- src/components/FetchCRUD/Fetch.jsx
+++ src/components/FetchCRUD/Fetch.jsx
@@ -1,35 +1,32 @@
-import React,{useState} from 'react'
-
+import React, { useState } from "react";
 
 const Fetch = () => {
-    const [id,setId]=useState()
+  const [id, setId] = useState();
 
-    const handleChange = event => {
-        setId(event.target.value)
+  const handleChange = (event) => {
+    setId(event.target.value);
+  };
+
+  const fetchData = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch(`http://localhost:8080/Api/Users/${id}/delete`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      console.log("Todo ok");
+    } else {
+      console.log("Algo salió mal");
+      console.error(await response.text());
     }
-
-    const fetchData = async event => {
-        event.preventDefault()
-
-        const response = await fetch(`http://localhost:8080/api/users/?id=${id}`, {
-            method: 'DELETE'
-        })
-
-        if (response.ok){
-            console.log("Todo ok")
-        } else {
-            console.log("Algo salió mal")
-            console.error( await response.text())
-        }
-    }
-    
-
+  };
   return (
     <form onSubmit={fetchData}>
-        <input type="text" name="id" value={id} onChange={handleChange} />
-        <button>Consultar por {id}</button>
+      <input type="text" name="id" value={id} onChange={handleChange} />
+      <button>ID a eliminar {id}</button>
     </form>
-  )
-}
+  );
+};
 
-export default Fetch
+export default Fetch;
